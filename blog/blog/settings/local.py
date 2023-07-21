248b4f47c106ccd7d9c3e051d14b701a4495dbd7
Marcos Leiva <133277772+leimark91@users.noreply.github.com>
2023-07-21 16:39:15 -0300

    realice cambios

--- blog/blog/settings/local.py
+++ blog/blog/settings/local.py
@@ -10,7 +10,11 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog_info',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
