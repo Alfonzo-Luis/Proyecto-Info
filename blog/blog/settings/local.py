aa6aa149cec92d1033c20aacf1430275069c8965
Alfonzo-Luis <89166702+Alfonzo-Luis@users.noreply.github.com>
2023-08-01 14:48:53 -0300

    Revert "Luis marcos antonio leiva"

--- blog/blog/settings/local.py
+++ blog/blog/settings/local.py
@@ -10,11 +10,11 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blog_info',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':'blog_info',
+        'USER':'root',
+        'PASSWORD':'root',
+        'HOST':'localhost',
+        'PORT':'',
     }
 }
